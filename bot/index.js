201a57cb49b00755d1aeb698aa1b7ae51b68205b
Bet-Zero <driertoaster1@gmail.com>
2025-08-05 21:46:37 -0500

    refactor(bot): switch to interaction handler

--- bot/index.js
+++ bot/index.js
@@ -1,28 +1,27 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
+/* eslint-env node */
+import { Client, GatewayIntentBits, Events } from "discord.js";
+import dotenv from "dotenv";
+import process from "node:process";
+
+dotenv.config();
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+  intents: [GatewayIntentBits.Guilds],
 });
 
-client.once("ready", () => {
+client.once(Events.ClientReady, () => {
   console.log(`🤖 Bot is online as ${client.user.tag}`);
 });
 
-client.on("messageCreate", (message) => {
-  if (message.content === "!ping") {
-    message.reply("🏓 Pong!");
-  }
-});
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-client.on("messageCreate", (message) => {
-  if (message.content === "/bzero") {
-    message.reply("Futures bot reporting for duty 📊");
+  if (interaction.commandName === "ping") {
+    await interaction.reply("🏓 Pong!");
+  } else if (interaction.commandName === "bzero") {
+    await interaction.reply("Futures bot reporting for duty 📊");
   }
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
