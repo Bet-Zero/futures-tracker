a5cc86182c8cd7a5539e981a437f6cad21882df9
Bet_Zero <driertoaster1@gmail.com>
2025-08-12 23:32:55 -0400

    New New

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import FuturesPage from "./pages/FuturesPage";
 import AddBetPage from "./pages/AddBetPage";
 
@@ -6,6 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/futures" replace />} />
         <Route path="/futures" element={<FuturesPage />} />
         <Route path="/add-bet" element={<AddBetPage />} />
       </Routes>
